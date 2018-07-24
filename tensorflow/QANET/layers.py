74efcbaaacf0d3aac27ea1020497e50cbb3674bf
LunaRobot <tulingbot@gmail.com>
2018-07-24 11:06:08 +0800

    ADD:QA-Net Model completed

--- tensorflow/QANET/layers.py
+++ tensorflow/QANET/layers.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import math
 
+from tensorflow.python.ops import nn_ops
+
 '''
 Some functions are taken directly from Tensor2Tensor Library:
 https://github.com/tensorflow/tensor2tensor/
@@ -392,10 +394,11 @@ def combine_last_two_dimensions(x):
 # 具体参看论文 https://arxiv.org/abs/1611.01603 中的Attention FLow layer
 # tf.tile()这个操作会把输入的tensor 的指定维度 扩张复制
 # 计算相似性矩阵 传递过来的args 中 包括 c, q,
+# 计算公式为 f(q,c)=W0[q,c,q⊙c]
 def optimized_trilinear_for_attention(args, c_maxlen, q_maxlen, input_keep_prob=1.0,
-                                      scope='efficient_trilinear',
-                                      bias_initializer=tf.zeros_initializer(),
-                                      kernel_initializer=initializer()):
+    scope='efficient_trilinear',
+    bias_initializer=tf.zeros_initializer(),
+    kernel_initializer=initializer()):
     assert len(args) == 2, "just use for computing attention with two input"
     arg0_shape = args[0].get_shape().as_list()
     arg1_shape = args[1].get_shape().as_list()
@@ -460,6 +463,9 @@ def ndim(x):
     return None
 
 
+# 多维度点积
+# 只需a矩阵的最后一维dim等于b矩阵倒数第二维dim即可，对应二维情况就是第一个的列数等于第二个矩阵行数
+# 也就是说点积发生在a,b矩阵最后两个维度上
 def dot(x, y):
     """Modified from keras==2.0.6
     Multiplies 2 tensors (and/or variables) and returns a *tensor*.
@@ -472,13 +478,13 @@ def dot(x, y):
     # Returns
         A tensor, dot product of `x` and `y`.
     """
-    if ndim(x) is not None and (ndim(x) > 2 or ndim(y) >2):
+    if ndim(x) is not None and (ndim(x) > 2 or ndim(y) > 2):
         x_shape = []
         for i, s in zip(x.get_shape().as_list(), tf.unstack(tf.shape(x))):
             if i is not None:
                 x_shape.append(i)
             else:
-                x_shape.append(x)
+                x_shape.append(s)
         x_shape = tuple(x_shape)
         y_shape = []
         for i, s in zip(y.get_shape().as_list(), tf.unstack(tf.shape(y))):
@@ -491,8 +497,8 @@ def dot(x, y):
         y_permute_dim = [y_permute_dim.pop(-2)] + y_permute_dim
         xt = tf.reshape(x, [-1, x_shape[-1]])
         yt = tf.reshape(tf.transpose(y, perm=y_permute_dim), [y_shape[-2], -1])
-        return tf.reshape(tf.matmul(xt, yt), x_shape[:-1] + y_shape[:-2] + y_shape[-1:])
-
+        return tf.reshape(tf.matmul(xt, yt),
+                          x_shape[:-1] + y_shape[:-2] + y_shape[-1:])
     if isinstance(x, tf.SparseTensor):
         out = tf.sparse_tensor_dense_matmul(x, y)
     else:
@@ -500,4 +506,69 @@ def dot(x, y):
     return out
 
 
+def batch_dot(x, y, axes=None):
+    """Copy from keras==2.0.6
+    Batchwise dot product.
+    `batch_dot` is used to compute dot product of `x` and `y` when
+    `x` and `y` are data in batch, i.e. in a shape of
+    `(batch_size, :)`.
+    `batch_dot` results in a tensor or variable with less dimensions
+    than the input. If the number of dimensions is reduced to 1,
+    we use `expand_dims` to make sure that ndim is at least 2.
+    # Arguments
+        x: Keras tensor or variable with `ndim >= 2`.
+        y: Keras tensor or variable with `ndim >= 2`.
+        axes: list of (or single) int with target dimensions.
+            The lengths of `axes[0]` and `axes[1]` should be the same.
+    # Returns
+        A tensor with shape equal to the concatenation of `x`'s shape
+        (less the dimension that was summed over) and `y`'s shape
+        (less the batch dimension and the dimension that was summed over).
+        If the final rank is 1, we reshape it to `(batch_size, 1)`.
+    """
+    if isinstance(axes, int):
+        axes = (axes, axes)
+    x_ndim = ndim(x)
+    y_ndim = ndim(y)
+    if x_ndim > y_ndim:
+        diff = x_ndim - y_ndim
+        y = tf.reshape(y, tf.concat([tf.shape(y), [1] * diff], axis=0))
+    elif y_ndim > x_ndim:
+        diff = y_ndim - x_ndim
+        x = tf.reshape(x, tf.concat([tf.shape(x), [1] * diff], axis=0))
+    else:
+        diff = 0
+    if ndim(x) == 2 and ndim(y) == 2:
+        if axes[0] == axes[1]:
+            out = tf.reduce_sum(tf.multiply(x, y), axes[0])
+        else:
+            out = tf.reduce_sum(tf.multiply(tf.transpose(x, [1, 0]), y), axes[1])
+    else:
+        if axes is not None:
+            adj_x = None if axes[0] == ndim(x) - 1 else True
+            adj_y = True if axes[1] == ndim(y) - 1 else None
+        else:
+            adj_x = None
+            adj_y = None
+        out = tf.matmul(x, y, adjoint_a=adj_x, adjoint_b=adj_y)
+    if diff:
+        if x_ndim > y_ndim:
+            idx = x_ndim + y_ndim - 3
+        else:
+            idx = x_ndim - 1
+        out = tf.squeeze(out, list(range(idx, idx + diff)))
+    if ndim(out) == 1:
+        out = tf.expand_dims(out, 1)
+    return out
 
+
+# 用于计算总参数个数
+def total_params():
+    total_parameters = 0
+    for variable in tf.trainable_variables():
+        shape = variable.get_shape()
+        variable_parametes = 1
+        for dim in shape:
+            variable_parametes *= dim.value
+        total_parameters += variable_parametes
+    print("Total number of trainable parameters: {}".format(total_parameters))
--- tensorflow/QANET/model.py
+++ tensorflow/QANET/model.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from .layers import conv, highway, residual_block, optimized_trilinear_for_attention, mask_logits
+from .layers import regularizer, conv, highway, residual_block, optimized_trilinear_for_attention, mask_logits, total_params
 
 
 class Model(object):
@@ -9,24 +9,23 @@ class Model(object):
         self.demo = demo
         self.graph = graph if graph is not None else tf.Graph()
         with self.graph.as_default():
+
             self.global_step = tf.get_variable('global_step', shape=[], dtype=tf.int32,
                                                initializer=tf.constant_initializer(0), trainable=False)
-
             self.dropout = tf.placeholder_with_default(0.0, (), name="dropout")
             if self.demo:
-                self.c = tf.placeholder(tf.int32, [None, config.test_para_limit], "context")
-                self.q = tf.placeholder(tf.int32, [None, config.test_ques_limit], "question")
-                self.ch = tf.placeholder(tf.int32, [None, config.test_para_limit, config.char_limit], "context_char")
-                self.qh = tf.placeholder(tf.int32, [None, config.test_ques_limit, config.char_limit], "question_char")
-                self.y1 = tf.placeholder(tf.int32, [None, config.test_para_limit], "answer_index1")
-                self.y2 = tf.placeholder(tf.int32, [None, config.test_para_limit], "answer_index2")
+                self.c = tf.placeholder(tf.int32, [None, config.test_para_limit],"context")
+                self.q = tf.placeholder(tf.int32, [None, config.test_ques_limit],"question")
+                self.ch = tf.placeholder(tf.int32, [None, config.test_para_limit, config.char_limit],"context_char")
+                self.qh = tf.placeholder(tf.int32, [None, config.test_ques_limit, config.char_limit],"question_char")
+                self.y1 = tf.placeholder(tf.int32, [None, config.test_para_limit],"answer_index1")
+                self.y2 = tf.placeholder(tf.int32, [None, config.test_para_limit],"answer_index2")
             else:
                 self.c, self.q, self.ch, self.qh, self.y1, self.y2, self.qa_id = batch.get_next()
-
-            self.word_mat = tf.get_variable("word_mat", initializer=tf.constant(word_mat, dtype=tf.float32),
-                                            trainable=False)
-            self.char_mat = tf.get_variable("char_mat", initializer=tf.constant(char_mat, dtype=tf.float32))
-
+            self.word_mat = tf.get_variable("word_mat", initializer=tf.constant(
+                word_mat, dtype=tf.float32), trainable=False)
+            self.char_mat = tf.get_variable(
+                "char_mat", initializer=tf.constant(char_mat, dtype=tf.float32))
             # 应该是先计算完长度 再转换为bool吧
             self.c_mask = tf.cast(self.c, tf.bool)
             self.q_mask = tf.cast(self.q, tf.bool)
@@ -46,17 +45,19 @@ class Model(object):
                 self.y1 = tf.slice(self.y1, [0, 0], [N, self.c_maxlen])
                 self.y2 = tf.slice(self.y2, [0, 0], [N, self.c_maxlen])
             else:
-                self.c_maxlen, self.q_maxlen, = config.para_limit, config.test_ques_limit
+                self.c_maxlen, self.q_maxlen = config.para_limit, config.ques_limit
 
-            self.ch_len = tf.reshape(tf.reduce_sum(tf.cast(tf.cast(self.ch, tf.bool), tf.int32), axis=2), [-1])
-            self.qh_len = tf.reshape(tf.reduce_sum(tf.cast(tf.cast(self.qh, tf.bool), tf.int32), axis=2), [-1])
+            self.ch_len = tf.reshape(tf.reduce_sum(
+                tf.cast(tf.cast(self.ch, tf.bool), tf.int32), axis=2), [-1])
+            self.qh_len = tf.reshape(tf.reduce_sum(
+                tf.cast(tf.cast(self.qh, tf.bool), tf.int32), axis=2), [-1])
 
             self.forward()
             total_params()
 
             if trainable:
-                self.lr = tf.minimum(config.learning_rate, 0.001 / tf.log(999.) *
-                                     tf.log(tf.cast(self.global_step, tf.float32) + 1))
+                self.lr = tf.minimum(config.learning_rate,
+                                     0.001 / tf.log(999.) * tf.log(tf.cast(self.global_step, tf.float32) + 1))
                 self.opt = tf.train.AdamOptimizer(learning_rate=self.lr, beta1=0.8, beta2=0.999, epsilon=1e-7)
                 grads = self.opt.compute_gradients(self.loss)
                 # zip(*) 为解压
@@ -104,10 +105,13 @@ class Model(object):
             q_emb = highway(q_emb, size=d, scope="highway", dropout=self.dropout, reuse=True)
 
         with tf.variable_scope("Embedding_Encoder_Layer"):
-            c = residual_block(c_emb, num_blocks=1, num_conv_layers=4, kernel_size=7, mask=self.c_mask, num_filters=d, num_heads=nh,
+            c = residual_block(c_emb, num_blocks=1, num_conv_layers=4, kernel_size=7, mask=self.c_mask, num_filters=d,
+                               num_heads=nh,
                                seq_len=self.c_len, scope="Encoder_Residual_Block")
-            q = residual_block(q_emb, num_blocks=1, num_conv_layers=4, kernel_size=7, mask=self.q_mask, num_filters=d, num_heads=nh,
-                               seq_len=self.q_len, scope="Encoder_Residual_Block", reuse=True,# Share the weights between passage and question
+            q = residual_block(q_emb, num_blocks=1, num_conv_layers=4, kernel_size=7, mask=self.q_mask, num_filters=d,
+                               num_heads=nh,
+                               seq_len=self.q_len, scope="Encoder_Residual_Block", reuse=True,
+                               # Share the weights between passage and question
                                bias=False, dropout=self.dropout)
 
         # 计算context-to-query attention和query-to-context attention矩阵。 1.
@@ -129,6 +133,77 @@ class Model(object):
             self.q2c = tf.matmul(tf.matmul(S_, S_T), c)
             attention_outputs = [c, self.c2q, c * self.c2q, c * self.q2c]
 
+        # 与BIDAF类似，每个位置的输入是[c,a,c⊙a,c⊙b]，a和b分别为attention矩阵A和B的行。
+        # 该层中encoder blocks的卷积层数为2，总blocks数为1，其余参数与embedding encoder layer相同。
+        # 三个model encoder之间共享参数。
+        with tf.variable_scope("Model_Encoder_Layer"):
+            inputs = tf.concat(attention_outputs, axis=-1)
+            self.enc = [conv(inputs, d, name="input_projection")]
+            for i in range(3):
+                if i % 2 == 0:  # dropout every 2 blocks
+                    self.enc[i] = tf.nn.dropout(self.enc[i], 1.0 - self.dropout)
+                self.enc.append(
+                    residual_block(self.enc[i],
+                                   num_blocks=7,
+                                   num_conv_layers=2,
+                                   kernel_size=5,
+                                   mask=self.c_mask,
+                                   num_filters=d,
+                                   num_heads=nh,
+                                   seq_len=self.c_len,
+                                   scope="Model_Encoder",
+                                   bias=False,
+                                   reuse=True if i > 0 else None,
+                                   dropout=self.dropout)
+                )
+        # 分别预测每个位置是answer span的起始点和结束点的概率，分别记为p1、p2，计算公式如下：
+        # p1=softmax(W1[M0;M1])；p2=softmax(W2[M0;M2])
+        # 其中&W_1&、&W_2&是两个可训练变量，M0、M1、M2依次对应结构图中三个model encoder block的输出（自底向上）。
+        # 目标函数：
+        # L(θ)=−1N∑iN[log(p1y1i)+log(p2y2i)]
+        # 其中y1i、y2i分别为第i个样本的groudtruth的起始位置和结束位置。
+        with tf.variable_scope("Output_Layer"):
+            start_logits = tf.squeeze(
+                conv(tf.concat([self.enc[1], self.enc[2]], axis=-1), 1, bias=False, name="start_pointer"), -1)
+            end_logits = tf.squeeze(
+                conv(tf.concat([self.enc[1], self.enc[3]], axis=-1), 1, bias=False, name="end_pointer"), -1)
+            self.logits = [mask_logits(start_logits, mask=self.c_mask),
+                           mask_logits(end_logits, mask=self.c_mask)]
 
+            logits1, logits2 = [l for l in self.logits]
 
+            outer = tf.matmul(tf.expand_dims(tf.nn.softmax(logits1), axis=2),
+                              tf.expand_dims(tf.nn.softmax(logits2), axis=1))
+            outer = tf.matrix_band_part(outer, 0, config.ans_limit)
+            self.yp1 = tf.argmax(tf.reduce_max(outer, axis=2), axis=1)
+            self.yp2 = tf.argmax(tf.reduce_max(outer, axis=1), axis=1)
+            losses = tf.nn.softmax_cross_entropy_with_logits(
+                logits=logits1, labels=self.y1)
+            losses2 = tf.nn.softmax_cross_entropy_with_logits(
+                logits=logits2, labels=self.y2)
+            self.loss = tf.reduce_mean(losses + losses2)
 
+        if config.l2_norm is not None:
+            variables = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+            l2_loss = tf.contrib.layers.apply_regularization(regularizer, variables)
+            self.loss += l2_loss
+
+        if config.decay is not None:
+            # https://blog.csdn.net/PKU_Jade/article/details/73477112
+            # 用来让模型不被更新的太快
+            self.var_ema = tf.train.ExponentialMovingAverage(config.decay)
+            ema_op = self.var_ema.apply(tf.trainable_variables())
+            # 构建的操作必须在ema_op之后执行才可以
+            with tf.control_dependencies([ema_op]):
+                self.loss = tf.identity(self.loss)
+                self.assign_vars = []
+                for var in tf.global_variables():
+                    v = self.var_ema.average(var)
+                    if v:
+                        self.assign_vars.append(tf.assign(var, v))
+
+    def get_loss(self):
+        return self.loss
+
+    def get_global_step(self):
+        return self.global_step
